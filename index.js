6a7d64b5da907c173af01bcfba8d3511fd25aa25
vasu.arya97@gmail.com <vasu.arya97@gmail.com>
2022-03-13 16:55:42 +0530

    latest code changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // const express = require("express"); // importing 3rd party packages
 import express from "express";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -82,7 +85,9 @@ app.use(express.json());
 
 // Create connection between DB and Node -
 
-const MONGO_URL = "mongodb://localhost";
+// const MONGO_URL = "mongodb://localhost";
+const MONGO_URL = process.env.MONGO_URL;
+
 async function createConnection() {
   const client = new MongoClient(MONGO_URL);
   await client.connect();
